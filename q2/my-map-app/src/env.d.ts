effd74683f28615a9af9aeb93e3b4fe20a32e758
tony.li <tony@choco-up.com>
2023-02-28 23:09:32 +0800

    chore(env): update env

--- q2/my-map-app/src/env.d.ts
+++ q2/my-map-app/src/env.d.ts
@@ -1,10 +1,11 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_PATH: string
-  readonly VITE_API_KEY: string
+  readonly VITE_API_PATH: string;
+  readonly VITE_API_KEY: string;
+  readonly VITE_MAP_KEY: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
