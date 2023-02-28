f8eaac0fef251114ae4992934249857f6d41af05
tony.li <tony@choco-up.com>
2023-02-28 23:09:11 +0800

    refactor(friends): add router

--- q2/my-map-app/src/App.tsx
+++ q2/my-map-app/src/App.tsx
@@ -1,8 +1,53 @@
-import { Suspense } from "react";
-import Friends from "./components/Friends";
+import { createContext, useState, useContext } from "react";
+import Friends from "./pages/Friends";
+import Friend from "./pages/Friend";
+import { defaultPeople, IPeople } from "./interfaces/api";
 
-export default () => (
-  <Suspense fallback="Loading friends...">
-    <Friends />
-  </Suspense>
-);
+import {
+  BrowserRouter,
+  Outlet,
+  Route,
+  Routes,
+  useOutletContext,
+} from "react-router-dom";
+
+export type PeopleContextType = {
+  people: IPeople | null;
+  setPeople: (p: IPeople) => {};
+};
+
+const Container = () => {
+  const [people, setPeople] = useState<IPeople | null>(defaultPeople);
+  return (
+    <>
+      <Outlet context={{ people, setPeople }} />
+    </>
+  );
+};
+
+export default () => {
+  // const selectPeople = (people: IPeople) => {
+  //   console.log('Set people at App level', { people });
+  //   setPeople(people);
+  // }
+
+  return (
+    <>
+      {/* <Friends /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Container />}>
+            <Route path="/" element={<Friends />} />
+            <Route path="friend/:id" index element={<Friend />} />
+            {/* </Route> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* <Friend /> */}
+    </>
+  );
+};
+
+export function usePeople() {
+  return useOutletContext<PeopleContextType>();
+}
