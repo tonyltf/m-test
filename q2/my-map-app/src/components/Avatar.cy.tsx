3957b64d5d9f83906ba38c81f35813d6373d1460
tony.li <tony@choco-up.com>
2023-02-28 09:53:33 +0800

    feat(avatar): complete component and testing

--- q2/my-map-app/src/components/Avatar.cy.tsx
+++ q2/my-map-app/src/components/Avatar.cy.tsx
@@ -1,10 +1,13 @@
-/// <reference types="Cypress" />
+/// <reference types="cypress" />
 import Avatar from "./Avatar";
 
-describe('Avatar', () => {
+describe("Avatar", () => {
   it("uses name on the Avatar component", () => {
     cy.mount(<Avatar firstName="Tony" lastName="Li" />);
-    // cy.get('button').should('contains.text', 'Click me!')
-    cy.get('div').should('contains.text', 'TL');
+    cy.get("[data-cy=avatar]").should("contains.text", "TL");
   });
-})
+  it("uses picture on the Avatar component", () => {
+    cy.mount(<Avatar firstName="Tony" lastName="Li" picture="https://placebear.com/225/210" />);
+    cy.get("[data-cy=avatar]").children("img").should("have.length", 1);
+  });
+});
--- q2/my-map-app/src/components/Avatar.tsx
+++ q2/my-map-app/src/components/Avatar.tsx
@@ -1,15 +1,18 @@
 import styles from "../styles/avatar.module.css";
 
 const Avatar = ({
-  firstName = "",
-  lastName = "",
+  firstName = '',
+  lastName = '',
+  picture = '',
 }: {
   firstName: string;
   lastName: string;
+  picture?: string;
 }) => {
   return (
-    <div className={styles.avatar}>
-      {firstName?.[0].toUpperCase() + lastName?.[0]?.toUpperCase()}
+    <div data-cy="avatar" className={styles.avatar}>
+      {picture && <img src={picture} className={styles.avatarImg} />}
+      {!picture && <div className={styles.avatarText}>{firstName?.[0].toUpperCase() + lastName?.[0]?.toUpperCase()}</div>}
     </div>
   );
 };
