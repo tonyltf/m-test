04d6f56975f99d13d6cacfa14871fedd29fa3db9
tony.li <tony@choco-up.com>
2023-03-02 01:06:54 +0800

    feat(friends): Add return mechanism on http error 429

--- q2/my-map-app/src/pages/Friends.tsx
+++ q2/my-map-app/src/pages/Friends.tsx
@@ -8,23 +8,41 @@ import usePeople from "../hooks/People";
 
 import styles from "../styles/friends.module.css";
 import "react-placeholder/lib/reactPlaceholder.css";
+import Logger from "../utils/Logger";
 
 export default () => {
   const [peopleList, setPeopleList] = useState<IPeople[]>([]);
   const { setPersistedPeople } = usePeople(defaultPeople);
 
   useEffect(() => {
+    let retryCount = 0;
     const fetchPeopleLlist = async () => {
+      let response;
       try {
-        const response = await fetch(VITE_API_PATH, {
+        response = await fetch(VITE_API_PATH, {
           headers: {
             Authorization: `Bearer ${VITE_API_KEY}`,
           },
         });
+      } catch (e) {
+        Logger.error(e);
+      }
+
+      if (response?.ok) {
         const result = await response.json();
         setPeopleList(result);
-      } catch (e) {
-        console.error(e);
+      } else {
+        if (response?.status === 429) {
+          const resetTime = parseInt(response?.headers?.get("x-ratelimit-reset") || '0', 10) ;
+          let retryAfter: number;
+          if (resetTime > new Date().getTime() / 1000) {
+            retryAfter = resetTime * 1000 - new Date().getTime();
+          } else {
+            retryAfter = Math.pow(2, retryCount++) * 1000 + Math.random() * 1000 // exponential backoff;
+          }
+          Logger.log(`Reached rate limit, will be retry after ${retryAfter/1000} second(s)`);
+          setTimeout(fetchPeopleLlist, retryAfter);
+        }
       }
     };
     fetchPeopleLlist();
