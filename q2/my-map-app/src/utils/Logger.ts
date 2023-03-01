9240621c797831e3f0ea8dbc427a08deb69e19dc
tony.li <tony@choco-up.com>
2023-03-02 01:05:25 +0800

    feat(logger): add a logger simulate feature

---
+++ q2/my-map-app/src/utils/Logger.ts
@@ -0,0 +1,14 @@
+interface ILogger {
+  log: (...data: any[]) => void,
+  error: (...data: any[]) => void,
+}
+
+const Logger = (): ILogger => {
+  return {
+    log: console.log,
+    error: console.error,
+  };
+}
+
+export default Logger();
+
