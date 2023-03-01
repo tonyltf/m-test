d41e0cd088b0f651828c868cf25cf60231f3e6b1
tony.li <tony@choco-up.com>
2023-03-02 00:26:15 +0800

    test(friends): add more test case

--- q2/my-map-app/cypress/e2e/Friends.cy.tsx
+++ q2/my-map-app/cypress/e2e/Friends.cy.tsx
@@ -1,12 +1,32 @@
-import Friends from "../../src/pages/Friends";
 
 describe("Friends", () => {
-  it("display list frieneds with mock data", () => {
+
+  beforeEach(() => {
     cy.intercept("GET", "/templates/-xdNcNKYtTFG/data", {
       fixture: "friend.json",
     });
     cy.visit("http://localhost:5173/");
+  });
+
+
+  it("display list friends with mock data", () => {
     cy.get("[data-cy=avatar]").children().should("have.length", 1);
     cy.get("[data-cy=card]").should("contain.text", "Tony Li");
   });
+
+  it("show detail friend info with mock data with clicked", () => {
+    cy.get("[data-cy=card]").click();
+    cy.url().should('include', '/friend/ae736d8f-5a08-4bab-8e30-1eb2079e5dc2');
+    cy.get("[data-cy=card]").should("contain.text", "Tony Li");;
+  });
+
+
+  it("show keeps in the detail page info after reload", () => {
+    cy.get("[data-cy=card]").click();
+    cy.url().should('include', '/friend/ae736d8f-5a08-4bab-8e30-1eb2079e5dc2');
+    cy.reload();
+    cy.url().should('include', '/friend/ae736d8f-5a08-4bab-8e30-1eb2079e5dc2');
+    cy.get("[data-cy=card]").should("contain.text", "Tony Li");
+  });
+
 });
